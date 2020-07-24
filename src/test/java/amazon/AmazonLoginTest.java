fefa70795695ca07745aafb34aeeecab0cbd119d
Sergey Ushatov <sergeykoapp@gmail.com>
2020-07-24 10:25:17 +0300

    amazon Test refactoring

--- src/test/java/amazon/AmazonLoginTest.java
+++ src/test/java/amazon/AmazonLoginTest.java
@@ -1,11 +1,10 @@
 package amazon;
 
-import org.hamcrest.MatcherAssert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
 
 public class AmazonLoginTest extends TestBase {
 
@@ -17,14 +16,13 @@ public class AmazonLoginTest extends TestBase {
     @Test
     public void loginTest() {
         getAmazonLoginHelper().login();
-
-        MatcherAssert.assertThat(getAmazonLoginHelper().isUserLoggedIn(), is(true));
+        assertThat(getAmazonLoginHelper().isUserLoggedIn(), is(true));
     }
 
     @Test
     public void logoutTest() {
         getAmazonLoginHelper().login();
         getAmazonLoginHelper().logout();
-        MatcherAssert.assertThat(getAmazonLoginHelper().isUserLoggedIn(), is(false));
+        assertThat(getAmazonLoginHelper().isUserLoggedIn(), is(false));
     }
 }
